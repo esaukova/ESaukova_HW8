8696b1d7c16c312733de873ac90809df116420f5
Ekaterina Saukova <esaukova@outlook.com>
2023-07-10 21:44:52 +0300

    Post

---
+++ src/main/java/ru/netology/esaukova/hw8/objects/FormDate.java
@@ -0,0 +1,8 @@
+package ru.netology.esaukova.hw8.objects;
+
+public class FormDate {
+    public int day; // день рождения
+    public int month; // месяц рождения
+    public int year; // год рождения
+
+}
---
+++ src/main/java/ru/netology/esaukova/hw8/objects/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.esaukova.hw8.objects;
+public class Post {
+    public FormDate birthday; //день рождения с типом FormDate
+    public String name; // поле для имени
+    public String passport; // поле для номера паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // согласен ли пользователь получать новости о скидках в виде sms
+
+}
